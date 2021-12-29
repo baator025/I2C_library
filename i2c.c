0e3012744b8c56606eab8111fa82906a0224aa42
baator025 <baator025@gmail.com>
2021-12-29 21:30:13 +0100

    added functions describing low-level i2c operations. Increased maximum chunk size.

--- i2c.c
+++ i2c.c
@@ -1,4 +1,4 @@
-#include <avr/io.h>
+#include "i2c.h"
 
 void i2c_init(void){
 	TWCR |= (1<<TWINT);
@@ -9,13 +9,36 @@ void i2c_init(void){
 	TWBR = 12;										//bit rate register value
 }
 
-uint8_t i2c_call(uint8_t address){
+void i2c_send_start(void){
 	TWCR |= (1<<TWINT);
-	TWCR |= (1<<TWSTA);										//send start bit
+	TWCR |= (1<<TWSTA);
 	while (! (TWCR & (1<<TWINT)));
-	TWDR = 2*address;										//transmit SLA+R/W, for write SLA+W=2*SLA, for read SLA+R=2*SLA+1
+}
+
+void i2c_send_stop(void){
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+	while (! (TWCR & (1<<TWSTO)));
+}
+
+void i2c_send_address(uint8_t address, char direction){
+	if(direction == 'w'){
+		TWDR = 2*address;									//transmit SLA+R/W, for write SLA+W=2*SLA, 
+	}else if(direction == 'r'){
+		TWDR = 2*address + 1;								//for read SLA+R=2*SLA+1
+	}
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	while (! (TWCR & (1<<TWINT)));
+}
+
+void i2c_send_byte(uint8_t data){
+	TWDR = data;											//send data
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (! (TWCR & (1<<TWINT)));
+}
+
+uint8_t i2c_call(uint8_t address){
+	i2c_send_start();
+	i2c_send_address(address, 'w');
 	if((TWSR&0xF8) == 0x18){								//check ACK bit, 0x18 for transmiter, 0x40 for receiver
 		TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 		while (! (TWCR & (1<<TWSTO)));
@@ -42,41 +65,25 @@ uint8_t i2c_scan(void){
 }
 
 uint8_t i2c_write(uint8_t address, uint8_t data){
-	TWCR |= (1<<TWINT);
-	TWCR |= (1<<TWSTA);										//send start bit
-	while (! (TWCR & (1<<TWINT)));
-	TWDR = 2*address;										//transmit SLA+W
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (! (TWCR & (1<<TWINT)));
-	//if((TWSR&0xF8) == 0x18){								//check ACK bit, 0x18 for transmiter
-	TWDR = data;										//send data
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (! (TWCR & (1<<TWINT)));
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);			//send stop bit
-	while (! (TWCR & (1<<TWSTO)));
-	//}
+	i2c_send_start();
+	i2c_send_address(address, 'w');
+	i2c_send_byte(data);
+	i2c_send_stop();
 	return 0;
 }
 
-uint8_t i2c_send_chunk(uint8_t address, uint8_t size, uint8_t* data){
-	uint8_t i;
-	TWCR |= (1<<TWINT);
-	TWCR |= (1<<TWSTA);										//send start bit
-	while (! (TWCR & (1<<TWINT)));
-	TWDR = 2*address;										//transmit SLA+W
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (! (TWCR & (1<<TWINT)));
+uint8_t i2c_send_chunk(uint8_t address, uint16_t size, uint8_t* data){
+	uint16_t i;
+	i2c_send_start();
+	i2c_send_address(address, 'w');
 	
 	if((TWSR&0xF8) != 0x18)	return 'e';						//check ACK bit, 0x18 for transmiter
 	
 	for(i=0; i<size; i++){
-		TWDR = *(data + i);										//send data
-		TWCR = (1<<TWINT) | (1<<TWEN);
-		while (! (TWCR & (1<<TWINT)));
+		i2c_send_byte(*(data+i));
 	}
 	
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);			//send stop bit
-	while (! (TWCR & (1<<TWSTO)));
+	i2c_send_stop();
 	
 	return 0;
 }
--- i2c.h
+++ i2c.h
@@ -1,5 +1,11 @@
+#include<avr/io.h>
+
 void i2c_init(void);
+void i2c_send_start(void);
+void i2c_send_stop(void);
+void i2c_send_address(uint8_t address, char direction);
+void i2c_send_byte(uint8_t data);
 uint8_t i2c_call(uint8_t address);
 uint8_t i2c_scan(void);
 uint8_t i2c_write(uint8_t address, uint8_t data);
-uint8_t i2c_send_chunk(uint8_t address, uint8_t size, uint8_t* data);
+uint8_t i2c_send_chunk(uint8_t address, uint16_t size, uint8_t* data);
